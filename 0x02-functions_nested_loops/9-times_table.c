39fd5fb39e82b76bec368c7ed9aef31e39615cfb
Therry <adjeitherry@gmail.com>
2022-07-05 22:42:58 -0700

    timet

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,20 +16,20 @@ void times_table(void)
 
 		for (mult = 1; mult <= 9; mult++)
 		{
-		_putchar(',');
-		_putchar(' ');
-
-		prod = num * mult;
-
-		if (prod <= 9)
-
+			_putchar(',');
 			_putchar(' ');
 
-		else
+			prod = num * mult;
 
-			_putchar((prod / 10) + '0');
+			if (prod <= 9)
+
+				_putchar(' ');
+
+			else
+
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
-		_putchar((prod % 10) + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
