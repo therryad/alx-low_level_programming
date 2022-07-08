13e6ce620ddd4bd7d480e0bff3271f26710eb482
Therry <adjeitherry@gmail.com>
2022-07-07 22:39:27 -0700

    final print

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,29 +8,22 @@
 
 void print_number(int n)
 {
-	unsigned int s, c, count;
+	unsigned int s;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		s = n * -1;
+		s = -n;
+		_putchar('-');
 	}
 	else
 	{
 		s = n;
 	}
 
-	c = s;
-	count = 1;
-
-	while (c < 9)
+	if (s / 10)
 	{
-		c /= 10;
-		count *= 10;
+		print_number(s / 10);
 	}
 
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((s / count) % 10) + 48);
-	}
+	_putchar((s % 10) + '0');
 }
