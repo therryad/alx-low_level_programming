3e5d6492e151ff61da3bf43f859dbc106cbe4408
Therry <adjeitherry@gmail.com>
2022-07-26 18:46:45 -0700

    done

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,11 +10,9 @@
 
 void free_grid(int **grid, int height)
 {
-	int i = 0;
+	int i;
 
-	if (grid == NULL)
-		return;
-	while (i < height)
-		free((int *)grid[i++]);
 	free(grid);
+	for (i = 0; i < height; i++)
+		free(grid[i]);
 }
